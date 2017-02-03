f7f8f49fb462c2189a3c87ee88544070cb391621
pitonisaX <karreyes@gmail.com>
2017-02-02 20:47:25 -0800

    added right imports and tests to verify db are working

--- assign6/user/views.py
+++ assign6/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
